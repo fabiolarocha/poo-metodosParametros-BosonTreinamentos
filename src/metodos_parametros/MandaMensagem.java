49d87f901821439db4832c53fc5b46dced4c153e
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-06-25 20:04:20 -0300

    Alterações no texto

--- src/metodos_parametros/MandaMensagem.java
+++ src/metodos_parametros/MandaMensagem.java
@@ -2,7 +2,7 @@ package metodos_parametros;
 
 public class MandaMensagem {
 	public void enviaMensagem() {
-		System.out.println("Bem-vindo à Boson Treinamentos ");
+		System.out.println("Bem-vindo a Boson Treinamentos ");
 	}
 	public void mandaBoasVindas(String nome, int idade) {
 		System.out.printf("Olá, %s, você tem %d anos de idade", nome, idade);
